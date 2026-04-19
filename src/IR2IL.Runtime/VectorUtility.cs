5eb7d7423354eb4094fc3d501e76a61eb2b3a248
Tim Jones <tim@timjones.io>
2026-04-19 14:06:46 +0100

    Implement cosh

--- src/IR2IL.Runtime/VectorUtility.cs
+++ src/IR2IL.Runtime/VectorUtility.cs
@@ -336,6 +336,14 @@ public static class VectorUtility
         return Vector256.Create(Math.Atan(vector[0]), Math.Atan(vector[1]), Math.Atan(vector[2]), Math.Atan(vector[3]));
     }
 
+    // Cosh
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector128<double> CoshV2F64(Vector128<double> vector)
+    {
+        return Vector128.Create(Math.Cosh(vector[0]), Math.Cosh(vector[1]));
+    }
+
     // Atan2
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
